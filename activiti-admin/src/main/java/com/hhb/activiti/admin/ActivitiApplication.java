5a63dfbe9a2f3d60decdd369fca289f8021a8155
Mr.Bean <876795091@qq.com>
2020-09-25 00:47:38 +0800

    1

---
+++ activiti-admin/src/main/java/com/hhb/activiti/admin/ActivitiApplication.java
@@ -0,0 +1,16 @@
+package com.hhb.activiti.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mr.Bean
+ * @date 2020/9/24 23:02
+ */
+@SpringBootApplication
+public class ActivitiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiApplication.class, args);
+	}
+}
